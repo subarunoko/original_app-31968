56f0529e00364913208738f3e93ec30049d9917f
subarunoko <kazenonakasubaru@yahoo.co.jp>
2021-08-29 20:42:03 +0900

    ユーザー結合テスト

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -5,14 +5,12 @@ FactoryBot.define do
     random = Random.new
     rnd_language = random.rand(1..16)
     rnd_description = Faker::Lorem.sentence
-
     # description           {"text"}  
     description           {rnd_description}
     language_id           {rnd_language}
 
-    # after(:build) do |item|
-    #   # item.image.attach(io: File.open("public/images/test_sample1.png"), filename: "test_image.png")
-    #   item.images.attach(io: File.open("public/images/test_sample1.png"), filename: "test_image.png")
-    # end
+    after(:build) do |profile|
+      profile.image.attach(io: File.open("public/images/test_profile1.png"), filename: "test_profile1_image.png")
+    end
   end
 end
